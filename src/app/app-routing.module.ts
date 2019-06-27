7237e5d1586ac3d98099ef4da7fd72cc1b01fe31
hnakae <nakae27@gmail.com>
2019-06-26 21:04:21 -0700

    responsive nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/site/home/home.component';
+import { AboutComponent } from './components/site/about/about.component';
+import { SkillsComponent } from './components/site/skills/skills.component';
+import { WorkComponent } from './components/site/work/work.component';
+import { ContactComponent } from './components/site/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'work', component: WorkComponent },
+  { path: 'contact', component: ContactComponent },
+  // { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidemenuComponent } from './components/layout/sidemenu/sidemenu.component';
+import { HomeComponent } from './components/site/home/home.component';
+import { AboutComponent } from './components/site/about/about.component';
+import { SkillsComponent } from './components/site/skills/skills.component';
+import { WorkComponent } from './components/site/work/work.component';
+import { ContactComponent } from './components/site/contact/contact.component';
+import { ContentComponent } from './components/layout/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidemenuComponent,
+    HomeComponent,
+    AboutComponent,
+    SkillsComponent,
+    WorkComponent,
+    ContactComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
