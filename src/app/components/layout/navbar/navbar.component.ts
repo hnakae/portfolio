c8a61bc521a0275bba647ea9644c5931425650ae
hnakae <nakae27@gmail.com>
2019-07-15 19:18:26 -0700

    add content to pages

--- src/app/components/layout/navbar/navbar.component.ts
+++ src/app/components/layout/navbar/navbar.component.ts
@@ -7,32 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   public  hideMobile = 'hide-mobile';
-  public open = '';
   path = '../../../../assets/images/custom-menu-icon.png';
 
   status = false;
 
 
   myfunction() {
-    // hideMobile 
+    // hide || reveal
     if (this.hideMobile === 'hide-mobile') {
       this.hideMobile = '';
     } else {
       this.hideMobile = 'hide-mobile';
     }
 
+    // menu || close : img
     if (this.path === '../../../../assets/images/custom-menu-icon.png') {
       this.path = '../../../../assets/images/custom-close.png';
     } else {
       this.path = '../../../../assets/images/custom-menu-icon.png';
     }
-
-    if (this.open === 'open') {
-      this.open = '';
-    } else {
-      this.open = 'open';
-    }
-
 
     this.status = !this.status;
   }
