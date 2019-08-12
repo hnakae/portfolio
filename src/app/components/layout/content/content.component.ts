90529262360d62885c5c737c0bebb1e0cf857a1b
hnakae <nakae27@gmail.com>
2019-08-12 16:22:16 -0700

    swipe to change pages

--- src/app/components/layout/content/content.component.ts
+++ src/app/components/layout/content/content.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import $ from 'jquery';
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
@@ -10,6 +10,26 @@ export class ContentComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // tslint:disable-next-line: only-arrow-functions
+    $(document).on('swipeleft swiperight', function(event) {
+      if (event.type === 'swiperight') {
+        const prevpage = '#' + $.mobile.activePage.prev('div[data-role="page"]')[0].id;
+        if (prevpage === '#home' || prevpage === '#blog' || prevpage === '#videos') {
+          $.mobile.changePage(prevpage, {
+            transition: 'slide',
+            reverse: true
+          });
+        }
+      }
+      if (event.type === 'swipeleft') {
+        const nextpage = '#' + $.mobile.activePage.next('div[data-role="page"]')[0].id;
+        if ( nextpage === '#videos' || nextpage === '#photos') {
+          $.mobile.changePage(nextpage, {
+            transition: 'slide',
+            reverse: false
+          });
+        }
+      }
+    });
   }
-
 }
