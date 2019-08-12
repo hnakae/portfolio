02bb5c0df316f8e7e7bf0a9a6e03b50df91b2de1
hnakae <nakae27@gmail.com>
2019-08-12 08:49:01 -0700

    clean up navbar.component.ts

--- src/app/components/layout/navbar/navbar.component.ts
+++ src/app/components/layout/navbar/navbar.component.ts
@@ -17,13 +17,7 @@ export class NavbarComponent implements OnInit {
       this.hideMobile = 'hide-mobile';
       this.path = '../../../../assets/images/custom-menu-icon.png';
     }
-    // this.hideMobile = 'hide-mobile';
   }
-
-  // showDesktop() {
-  //   this.hideMobile = '';
-  //   this.mode = 'desktop';
-  // }
 
   toggleMenu() {
     if (this.hideMobile === 'hide-mobile') {
@@ -36,28 +30,19 @@ export class NavbarComponent implements OnInit {
     } else {
       this.path = '../../../../assets/images/custom-menu-icon.png';
     }
-
   }
 
   WidthChange(mq) {
     if (mq.matches) {
     // window width is at least 1025px
-      $('nav').removeClass('hide');
       $('nav').addClass('show');
       $('ul').addClass('show');
       $('i').addClass('show');
-      
     } else {
     // window width is less than 1025px
-      // nav links invisible
       $('nav').removeClass('show');
-      // $('nav').addClass('hide');
       $('ul').addClass('hide');
       $('i').addClass('hide');
-      
-      // if (this.hideMobile === '') {
-      //   this.toggleMenu();
-      // }
     }
   }
 
@@ -66,13 +51,6 @@ export class NavbarComponent implements OnInit {
   ngOnInit() {
     if (matchMedia) {
       const mq = window.matchMedia('(min-width:1025px)');
-      // if (mq.matches) {
-      //   this.hideMobile = '';
-      // }
-      // const mq2 = window.matchMedia('(max-width:1024px)');
-      // if (mq2.matches) {
-      //   this.hideMobile = 'hide-mobile';
-      // }
       // tslint:disable-next-line: deprecation
       mq.addListener(this.WidthChange);
       this.WidthChange(mq);
