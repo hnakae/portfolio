bf03e08764c82c2d3291d823ef7ccefd24b60744
hnakae <nakae27@gmail.com>
2019-08-28 03:14:04 -0700

    now a progressive web app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,8 @@ import { ContactComponent } from './components/site/contact/contact.component';
 import { ContentComponent } from './components/layout/content/content.component';
 import { GmapComponent } from './components/api/gmap/gmap.component';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 
 @NgModule({
@@ -36,7 +38,8 @@ import { LocationStrategy, HashLocationStrategy } from '@angular/common';
     AgmCoreModule.forRoot({
       // please dont use this key lol
       apiKey: 'AIzaSyBEkdHuhopLCGmsNAOUJiusigM5AOJOMUk'
-    })
+    }),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [
     {provide: LocationStrategy, useClass: HashLocationStrategy}
